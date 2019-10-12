7b558162558700bd985495ee2e6763ca9cbe09aa
amit kamboj <amitkamboj9034@gmail.com>
2019-10-11 23:58:33 -0700

    initial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return ( 
+        <div className = "App" >
+        <h1 > Welcome to my application </h1> 
+        <h2 > Amit  Kamboj </h2> 
+        <h2>013827489</h2> 
+        </div>
+    );
+}
+
+export default App;
